7b4bf38ea2e39c324cdb11a263b2b6c5adaf99fd
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-01-04 16:32:33 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 김정우 on 1/4/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
